9359d9cd771d54421f5edd89be17b0fda1c5927f
Ashish Gunjal <ashish.gunjal@mahindraholidays.com>
2021-02-16 15:48:34 +0530

    added slice

---
+++ JS.js
@@ -0,0 +1,6 @@
+// Slice in javascript
+
+var test = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
+var result = test.slice(1,3) // ["Tuesday", "Wednesday"]
+var result = test.slice(2,4) // ["Wednesday", "Thursday"]
+var result = test.slice(1) //  ["Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
